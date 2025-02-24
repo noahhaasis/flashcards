302ec61a18c8a9bbed127127786e3ab6c781562f
Noah Haasis <haasis_noah@yahoo.de>
2025-02-24 17:37:55 +0000

    Fix requirements and settings.py for the deployment

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-x131%=!j$i=km(3jp6gy$56o7q3-h*j=o77jbtyaal40ag0fcv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['159.69.1.243', '626words.com']
 
 
 # Application definition
