9120ebb99885d7f469114e5f428804a763192cd6
Noah Haasis <haasis_noah@yahoo.de>
2025-02-24 20:04:30 +0100

    Move more secrets into .env

--- core/settings.py
+++ core/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x131%=!j$i=km(3jp6gy$56o7q3-h*j=o77jbtyaal40ag0fcv'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['159.69.1.243', '626words.com']
+ALLOWED_HOSTS = ['159.69.1.243', '626words.com', 'localhost']
 
 
 # Application definition
